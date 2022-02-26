e61e876ba1c0020af52bb7646812ab42b7f5181d
amit-lekhak <amitlekhak1@gmail.com>
2022-02-26 16:19:07 +0545

    Add Error Boundary

--- app/root.tsx
+++ app/root.tsx
@@ -1,43 +1,66 @@
-import type { LinksFunction } from "remix";
-import { Links, LiveReload, Outlet } from "remix";
+import type { LinksFunction } from 'remix';
+import { Links, LiveReload, Outlet } from 'remix';
 
-import globalStylesUrl from "./styles/global.css";
-import globalMediumStylesUrl from "./styles/global-medium.css";
-import globalLargeStylesUrl from "./styles/global-large.css";
+import globalStylesUrl from './styles/global.css';
+import globalMediumStylesUrl from './styles/global-medium.css';
+import globalLargeStylesUrl from './styles/global-large.css';
 
 export const links: LinksFunction = () => {
   return [
     {
-      rel: "stylesheet",
-      href: globalStylesUrl
+      rel: 'stylesheet',
+      href: globalStylesUrl,
     },
     {
-      rel: "stylesheet",
+      rel: 'stylesheet',
       href: globalMediumStylesUrl,
-      media: "print, (min-width: 640px)"
+      media: 'print, (min-width: 640px)',
     },
     {
-      rel: "stylesheet",
+      rel: 'stylesheet',
       href: globalLargeStylesUrl,
-      media: "screen and (min-width: 1024px)"
-    }
+      media: 'screen and (min-width: 1024px)',
+    },
   ];
 };
 
-export default function App() {
+function Document({
+  children,
+  title = `Remix: So great, it's funny!`,
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <title>Remix: So great, it's funny!</title>
+        <meta charSet='utf-8' />
+        <title>{title}</title>
         <Links />
       </head>
       <body>
-        <Outlet />
-        {process.env.NODE_ENV === "development" ? (
-          <LiveReload />
-        ) : null}
+        {children}
+        <LiveReload />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document title='Uh-oh!'>
+      <div className='error-container'>
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+      </div>
+    </Document>
   );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,36 @@
-import type { LinksFunction } from "remix";
-import { Link } from "remix";
+import type { LinksFunction } from 'remix';
+import { Link } from 'remix';
 
-import stylesUrl from "~/styles/index.css";
+import stylesUrl from '~/styles/index.css';
 
 export const links: LinksFunction = () => {
   return [
     {
-      rel: "stylesheet",
-      href: stylesUrl
-    }
+      rel: 'stylesheet',
+      href: stylesUrl,
+    },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="container">
-      <div className="content">
+    <div className='container'>
+      <div className='content'>
         <h1>
           Remix <span>Jokes!</span>
         </h1>
         <nav>
           <ul>
             <li>
-              <Link to="jokes">Read Jokes</Link>
+              <Link to='jokes'>Read Jokes</Link>
             </li>
           </ul>
         </nav>
       </div>
     </div>
   );
+}
+
+export function ErrorBoundary() {
+  return <div className='error-container'>I did a whoopsies.</div>;
 }
